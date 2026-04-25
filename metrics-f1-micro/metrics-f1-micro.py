f8500348ea0381f643e0a413eda968e4d9494293
BOGDANCER1 <bogdan12042001@mail.ru>
2026-04-25 12:59:40 +0300

    Update solution for metrics-f1-micro

--- metrics-f1-micro/metrics-f1-micro.py
+++ metrics-f1-micro/metrics-f1-micro.py
@@ -4,7 +4,7 @@ def f1_micro(y_true, y_pred) -> float:
     """
     correct = 0
     mistake = 0
-
+    # It's actually the same as accuracy
     for ground_truth, prediction in zip(y_true, y_pred):
         if ground_truth == prediction:
             correct += 1
