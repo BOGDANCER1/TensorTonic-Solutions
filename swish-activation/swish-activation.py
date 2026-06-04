db119445d803cd353a9d64d40715446b2ed4c0b1
BOGDANCER1 <bogdan12042001@mail.ru>
2026-06-04 15:01:16 +0300

    Update solution for swish-activation

--- swish-activation/swish-activation.py
+++ swish-activation/swish-activation.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-def swish(x: list) -> np.ndarray:
+def swish(x):
     """
     Implement Swish activation function.
     """
-    if isinstance(x, float) or isinstance(x, int):
-        return np.array([x / (1 + np.exp(-x))])
-    x = np.array(x)
-    return x / (1 + np.exp(- x))
+    x = np.asarray(x)
+    return x / (1 + np.exp(-x))
